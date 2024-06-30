0bc5daaac27e733fde241da69050fd9e3043a4f1
Chance Jones <everettjones2001@gmail.com>
2024-06-30 14:43:26 -0700

    Update UserService.java
    Added the Spring Boot JDBC methods

--- Milestone-Registration/src/main/java/com/gcu/UserService.java
+++ Milestone-Registration/src/main/java/com/gcu/UserService.java
@@ -2,10 +2,12 @@ package com.gcu;
 
 public class UserService implements UserInterface {
 
+	@Autowired
+    	private UserRepository userRepository;
+	
 	@Override
 	public void register(UserModel model) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
@@ -19,5 +21,13 @@ public class UserService implements UserInterface {
 		// TODO Auto-generated method stub
 		System.out.println("Destroy");
 	}
+
+   	public User registerUser(UserModel user) {
+        	return userRepository.save(user);
+    	}
+
+    	public User findByUsername(String username) {
+        	return userRepository.findByUsername(username);
+    	}
 
 }
